590facc519124095f6dbed50bb399d4c6a2eb335
epoluodi <12163687@qq.com>
2016-01-25 18:13:06 +0800

    主界面下面OK ，appico OK

---
+++ Remote/tab1View.h
@@ -0,0 +1,13 @@
+//
+//  tab1View.h
+//  Remote
+//
+//  Created by Stereo on 16/1/25.
+//  Copyright © 2016年 com.cl.remote. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tab1View : UIView
+
+@end
---
+++ Remote/tab1View.m
@@ -0,0 +1,23 @@
+//
+//  tab1View.m
+//  Remote
+//
+//  Created by Stereo on 16/1/25.
+//  Copyright © 2016年 com.cl.remote. All rights reserved.
+//
+
+//tab1 界面
+
+#import "tab1View.h"
+
+@implementation tab1View
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
